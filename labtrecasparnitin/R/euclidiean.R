35bd547f4e34200a30062167eb55324bfc7a7cea
Nitin <datascience.nitinkumar@gmail.com>
2018-09-13 15:39:18 +0200

    commit one

---
+++ labtrecasparnitin/R/euclidiean.R
@@ -0,0 +1,13 @@
+euclidiean <-
+function(x1,x2){
+  if(x1>x2){
+    smaller<-x2
+  } else {
+    smaller<-x1
+  }
+  for(i in 1:smaller){
+    if((x1%%i==0)&&(x2%%i==0)){
+      hcf=i
+    }}
+  return(hcf)
+}
